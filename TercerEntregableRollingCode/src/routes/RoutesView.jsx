ce0ae851ef7e8c9d86ed31fbe68af6c0fe712bba
unknown <lucasrodrigopregotburiek@gmail.com>
2023-09-07 17:30:58 -0300

    creo UserPage

--- TercerEntregableRollingCode/src/routes/RoutesView.jsx
+++ TercerEntregableRollingCode/src/routes/RoutesView.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 import {Routes, Route} from 'react-router-dom'
 import AdminPage from '../pages/AdminPage'
-
 import Home from '../pages/Home'
 import ProductPage from '../pages/ProductPage'
-
 import RecordPage from '../pages/RecordPage'
+import UserPage from '../pages/userPage'
 
 
 
@@ -16,7 +15,7 @@ const RoutesView = () => {
     <Routes>
       <Route path='/' element={<Home/>} />
         <Route path='/adminPage' element={<AdminPage/>} />
-        
+        <Route path='/userpage' element={<UserPage/>} />
         <Route  path='/product/:id' element={<ProductPage/>}/>
         <Route  path='/record/:id' element={<RecordPage/>}/>
     </Routes>
