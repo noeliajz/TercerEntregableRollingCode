86ccdfc6c72dd66eeaa1ade0c8d0c182a61ca90a
unknown <lucasrodrigopregotburiek@gmail.com>
2023-09-12 11:37:40 -0300

    agrego carrousel

--- TercerEntregableRollingCode/src/pages/UserPage.jsx
+++ TercerEntregableRollingCode/src/pages/UserPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import CardProduct from '../components/CardProduct'
+import CarrouselPublicity from '../components/CarrouselPublicity'
 
 const UserPage = () => {
   return (
     <>
+    <div className="conteiner">
+      <div className="row">
+      <CarrouselPublicity/>
     <CardProduct/>
+      </div>
+    </div>
+   
     </>
   )
 }
