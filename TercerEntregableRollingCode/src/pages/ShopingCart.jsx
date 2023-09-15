304dd6d40a656a4b0f2dce2defc46adbf9db1d3c
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2023-09-14 23:12:32 -0300

    se agregan la opción Carrito al menú de usuario logueado

--- TercerEntregableRollingCode/src/pages/ShopingCart.jsx
+++ TercerEntregableRollingCode/src/pages/ShopingCart.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ShopingCart = () => {
+  const [products, setProducts] = useState([])
+  const getAllProducts = async () => {
+    const res = await axios.get('http://localhost:8080/api/products')
+    const  {getAllProd}= res.data
+    setProducts(getAllProd)}
+ 
+    useEffect(() => {
+    
+        getAllProducts()
+    }, [])
   return (
     <>
       <h1>Carrito</h1>
