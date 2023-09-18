304186da00da6814423bb449d2fae33cd628e031
evelinld <evelin5damato@gmail.com>
2023-09-18 23:36:27 +0200

    se agregan cambios a sobre nosotros

--- TercerEntregableRollingCode/src/routes/RoutesView.jsx
+++ TercerEntregableRollingCode/src/routes/RoutesView.jsx
@@ -16,6 +16,7 @@ import SobreNosotros from '../pages/SobreNosotros'
 import FirstPlan from '../pages/FirstPlan'
 import SecondPlan from '../pages/SecondPlan'
 import ThirdPlan from '../pages/thirdPlan'
+import Datepicker from '../pages/Datepicker'
 
 const RoutesView = ({setUserAdmin}) => {
   return (
@@ -35,6 +36,7 @@ const RoutesView = ({setUserAdmin}) => {
         <Route  path='/firstplan' element={<FirstPlan/>}/>
         <Route  path='/secondplan' element={<SecondPlan/>}/>
         <Route  path='/thirdplan' element={<ThirdPlan/>}/>
+        <Route  path='/datepicker' element={<Datepicker/>}/>
     </Routes>
   )
 }
